45105c7d3c68030a0e6be95d8e1380779708ed14
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2026-06-15 11:45:41 +0600

    refactor(PricingView): replace PricingSection and CTASection with PricingHIW and FinalCTASection for improved structure

--- src/pages/public/prublic_pricing/PricingView.jsx
+++ src/pages/public/prublic_pricing/PricingView.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import PricingSection from '../prublic_home/components/PricingSection';
-import CTASection from '../prublic_home/components/CTASection';
+import PricingHIW from '../prublic_home/components/how-it-works/PricingSection';
+import { FinalCTASection } from '../prublic_home/components/how-it-works/FinalCTASection';
 
 const PricingView = () => {
   return (
     <div className="w-full">
-      <PricingSection />
-      <CTASection />
+      <PricingHIW />
+      <div className="mx-auto max-w-385 px-5 pb-12.5 lg:px-20 lg:pb-22.5">
+        <FinalCTASection />
+      </div>
     </div>
   );
 };
