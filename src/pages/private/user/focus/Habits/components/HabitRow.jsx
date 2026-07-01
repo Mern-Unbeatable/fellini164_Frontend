59a92add488c4a7ffe75456a6759491160a06052
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2026-07-01 12:15:26 +0600

    Refactor HabitRow component layout by adjusting flex alignment for improved responsiveness

--- src/pages/private/user/focus/Habits/components/HabitRow.jsx
+++ src/pages/private/user/focus/Habits/components/HabitRow.jsx
@@ -205,7 +205,7 @@ export default function HabitRow({
 
       {isCompleted ? (
         <div
-          className={`flex items-center justify-between max-lg:w-full max-lg:pr-0 ${
+          className={`flex items-start justify-between max-lg:w-full max-lg:pr-0 ${
             compact ? 'w-[460px] shrink-0' : `flex-1 ${showMenu ? 'pr-8 2xl:pr-41' : ''}`
           }`}
         >
@@ -216,7 +216,7 @@ export default function HabitRow({
         </div>
       ) : (
         <div
-          className={`flex items-center max-lg:grid max-lg:w-full max-lg:grid-cols-7 max-lg:place-items-center max-lg:gap-1 max-lg:pr-0 ${
+          className={`flex items-start max-lg:grid max-lg:w-full max-lg:grid-cols-7 max-lg:place-items-center max-lg:gap-1 max-lg:pr-0 ${
             compact ? 'w-[460px] shrink-0 justify-end gap-5' : `flex-1 justify-between ${showMenu ? 'pr-8 2xl:pr-41' : ''}`
           }`}
         >
