0fbf98558d8fbcd4f15a9e622c4124d51a519305
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2026-07-01 11:26:26 +0600

    Remove onClick handler from TaskFormModal backdrop to prevent unintended closure

--- src/pages/private/user/focus/Tasks/components/TaskFormModal.jsx
+++ src/pages/private/user/focus/Tasks/components/TaskFormModal.jsx
@@ -481,7 +481,6 @@ export default function TaskFormModal({ mode = 'create', initialTask, onClose, o
 
   return (
     <div
-      onClick={onClose}
       className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
     >
       <div
