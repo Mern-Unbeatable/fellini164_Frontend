b2c55e9cb6b4b5cf12893c1beca019007ac31bc1
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2026-07-04 11:42:42 +0600

    clean code

--- src/pages/private/admin/management/waitingListComponents/ThreeDot.jsx
+++ src/pages/private/admin/management/waitingListComponents/ThreeDot.jsx
@@ -60,84 +60,13 @@ const ThreeDot = ({ setOpenMenuId, openMenuId, user, handleToggleSubscription, u
                 )}
               </button>
             </div>
-            {/* <div className="text-start">
-              <label className="mb-0.5 text-base font-normal text-gray-500" htmlFor="">
-                Referral id
-              </label>
-              <input
-                type="text"
-                defaultValue=""
-                placeholder="Referral id"
-                className="rounded-lg border border-gray-200 py-2 text-center text-base outline-none focus:ring-2 focus:ring-indigo-500"
-              />
-            </div> */}
-
-            {/* <div className="">
-              <button
-                className={`sm:text-base} flex w-full items-center justify-center gap-1 bg-[#7C3AED] px-4 py-2 text-center text-sm font-medium text-[#fff] shadow-lg`}
-              >
-                Referral now
-              </button>
-            </div> */}
-
-            {/* Edit Section */}
-            <div className="block w-full text-left">
-              {/* <button
-                onClick={() => setIsModalOpen(true)}
-                className="text-start font-medium text-gray-700 hover:underline"
-              >
-                Edit User
-              </button> */}
-            </div>
+           
+           
           </div>
         </div>
       )}
 
-      {/* Modal */}
-      {/* {isModalOpen && (
-        <EditModal
-          isOpen={isEditModalOpen}
-          closeEditModal={closeEditModal}
-          onClose={() => setIsEditModalOpen(false)}
-          user={user}
-        >
-          <div className="p-4">
-            <div className="mb-4 flex items-center justify-between">
-              <h2 className="text-lg font-semibold">Edit User</h2>
-            </div>
-
-            <form className="flex flex-col gap-3">
-              <input
-                type="text"
-                defaultValue={user.name}
-                placeholder="Name"
-                className="rounded border p-2"
-              />
-
-              <input
-                type="text"
-                defaultValue={user.location}
-                placeholder="Location"
-                className="rounded border p-2"
-              />
-
-              <input
-                type="tel"
-                defaultValue={user.mobile}
-                placeholder="Mobile Number"
-                className="rounded border p-2"
-              />
-
-              <button
-                type="submit"
-                className="rounded bg-blue-600 px-4 py-2 text-white hover:opacity-90"
-              >
-                Save
-              </button>
-            </form>
-          </div>
-        </EditModal>
-      )} */}
+     
     </div>
   );
 };
